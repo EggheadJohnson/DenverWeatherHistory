ba52b148a30b0768f8314bfc863365148ec123ac
Paul Johnson <PaulE.Johnson@gmail.com>
2020-07-25 20:50:09 -0600

    updating selectomatic to be py3 and good work on the charts

--- display_chart.py
+++ display_chart.py
@@ -1,6 +1,7 @@
 from selectomatic import selectomatic
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def displayChart(weatherData):
@@ -17,3 +18,56 @@ def displayChart(weatherData):
 
     extrema = ['Highs', 'Lows', 'Exit']
     periods = ['Month', 'Year', 'Month and Year', 'Forever', 'Exit']
+
+def loadPandasDF(file_name = 'data/Denver20192020.csv'):
+    df = pd.read_csv(file_name)
+    return df
+
+def filterDFToHaveTMAX(df):
+    return df[df.TMAX.notna()]
+
+def fetchAndFilter(file_name = 'data/Denver20192020.csv'):
+    return filterDFToHaveTMAX(loadPandasDF(file_name))
+
+def selectYearAndMonthFromDataframe(df):
+    years = set([ d.split('-')[0] for d in df.DATE])
+    selected_year = selectomatic(list(years))
+    months = set( d.split('-')[1] for d in df[df.DATE.str.contains(selected_year)].DATE)
+    selected_month = selectomatic(list(months))
+    return selected_year, selected_month
+
+def sliceDataframeToYear(df, year = '2020'):
+    return df[df.DATE.str.contains(year)]
+
+def sliceDataframeToMonth(df, month = '01'):
+    return df[df.DATE.str.contains('-' + month + '-')]
+
+def sliceDataframeToYearAndMonth(df, year = '2020', month = None):
+    sliced_df = sliceDataframeToYear(df, year)
+    if month:
+        sliced_df = sliceDataframeToMonth(sliced_df, month)
+    return sliced_df
+
+def chartDataColumn(df, column='TMAX', title=None):
+    if not title:
+        title = column
+    ax = plt.gca()
+    df.plot(kind='line', x='DATE', y=column, ax=ax, title=title)
+    # plt.show()
+
+def calculateTmaxRollingMean(df, window=3, win_type=None):
+    df['rolling_mean'] = df['TMAX'].rolling(window, win_type=win_type).mean()
+    return df
+
+# print(fetchAndFilter().head(15))
+df = filterDFToHaveTMAX(loadPandasDF())
+print(df.head(15))
+year, month = selectYearAndMonthFromDataframe(df)
+sliced_df = sliceDataframeToYearAndMonth(df, year, month)
+print(sliced_df)
+sliced_df = calculateTmaxRollingMean(sliced_df)
+# print(sliced_df.columns)
+
+chartDataColumn(sliced_df, 'rolling_mean', 'poopy')
+chartDataColumn(sliced_df, 'TMAX', 'butthole')
+plt.show()
--- selectomatic.py
+++ selectomatic.py
@@ -3,18 +3,18 @@ import datetime
 def selectomatic(options, optionalSummaries = {}):
     found = False
     while not found:
-        print "Please make a selection by number, letter, or full text"
+        print("Please make a selection by number, letter, or full text")
         for i, option in enumerate(options):
             if option in optionalSummaries:
                 summary = optionalSummaries[option]
             else:
                 summary = ''
-            print "  " + str(i+1) + ": " + option + " " + summary
-        selection = raw_input(" Selection: ")
+            print("  " + str(i+1) + ": " + option + " " + summary)
+        selection = input(" Selection: ")
         if selection.isdigit():
             selection = int(selection) - 1
             if selection >= len(options):
-                print "Invalid selection"
+                print("Invalid selection")
                 selection += 1
             else:
                 selection = options[selection]
@@ -28,12 +28,12 @@ def selectomatic(options, optionalSummaries = {}):
                     found = True
                     break
             if not found:
-                print "Invalid selection"
+                print("Invalid selection")
         elif selection not in options:
-            print "Invalid selection"
+            print("Invalid selection")
         elif selection in options:
             found = True
-        print "Your selection was " + str(selection)
+        print("Your selection was " + str(selection))
     return selection
 
 def selectMonth():
@@ -44,7 +44,7 @@ def selectMonth():
 def selectYear(oldest = 1980):
     year = -1
     while not validYear(year, oldest):
-        year = raw_input(" Select year: " )
+        year = input(" Select year: " )
     return year
 
 
